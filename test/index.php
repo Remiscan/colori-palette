996cf018249771a727a243dd2152f49a7a0a4736
Rémi Scandella <remi.scandella@gmail.com>
2022-05-12 00:21:56 +0200

    Update versioning

--- test/index.php
+++ test/index.php
@@ -1,5 +1,25 @@
 <!doctype html>
+
+<!-- ▼ Fichiers cache-busted grâce à PHP -->
+<!--<?php ob_start();?>-->
+
+<!-- Import map -->
+<script defer src="/_common/polyfills/es-module-shims.js"></script>
+<script type="importmap">
+{
+  "imports": {
+    "colori": "/colori/dist/colori.min.js",
+    "palette": "/colori/palette/palette.js"
+  }
+}
+</script>
+
 <link rel="stylesheet" href="./styles.css">
+
+<!--<?php $imports = ob_get_clean();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/versionize-files.php';
+echo versionizeFiles($imports, __DIR__); ?>-->
+
 <style>
   .palette {
     display: flex;
@@ -29,15 +49,8 @@
 </p>
 
 <script type="module">
-  // ▼ ES modules cache-busted grâce à PHP
-  /*<?php ob_start();?>*/
-
-  import Couleur from '../../dist/colori.js';
-  import Palette from '../palette.js';
-
-  /*<?php $imports = ob_get_clean();
-  require_once $_SERVER['DOCUMENT_ROOT'] . '/_common/php/versionize-files.php';
-  echo versionizeFiles($imports, __DIR__); ?>*/
+  import Couleur from 'colori';
+  import Palette from 'palette';
 
 
 
